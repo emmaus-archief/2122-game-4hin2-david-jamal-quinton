4037186bb08606f916876345e2b2dd344675f3c0
jamalednII <93325223+jamalednII@users.noreply.github.com>
2022-06-12 14:55:35 +0000

    jamal gedeelte af

--- script.js
+++ script.js
@@ -15,104 +15,157 @@ const GAMEOVER = 2;
 var spelStatus = SPELEN;
 const Pos = [580,310,70]; 
 var Omhoog = false
-var OmhoogTwee
+var OmhoogT
 var Omlaag = false
-var OmlaagTwee 
-var spelerX = 270 // x-positie van speler
-var spelerY = 580 // y-positie van speler\
-var vijandX = 1250
-var vijandY =  Pos [Math.floor(Math.random() * Pos.length)]
-var healthX = 1250
-var healthY = 580
-var HP = 10
-var vG = false 
-var vM = false
+var OmlaagT 
+var sX = 270 // x-positie van s
+var sY = 580 // y-positie van s\
+var vX = 1250
+var vY =  Pos [Math.floor(Math.random() * Pos.length)]
+var vXT = 1400
+var vYT = Pos [Math.floor(Math.random() * Pos.length)]
+var vXD = 1400
+var vYD = Pos [Math.floor(Math.random() * Pos.length)]
+var hX = 1250
+var hY =  Pos [Math.floor(Math.random() * Pos.length)]
+var hXT = 1350
+var hYT =  Pos [Math.floor(Math.random() * Pos.length)]
+var HP = 25
 var img
+var imgT
 var imgX = 0
-var PosTwee = false
-
-  
-  
-
-
+var imgTX = 0
+var imgTY
 
 /* ********************************************* */
 /* functies die je gebruikt in je game           */
 /* ********************************************* */
 
 /**
- * Updatet globale variabelen met posities van speler, vijanden en kogels
+ * Updatet globale variabelen met posities van s, ven en kogels
  */
-var beweegAlles = function () {
-  // speler
-  OmhoogTwee = Omhoog
+var begAlles = function () {
+  // s
+  OmhoogT = Omhoog
   Omhoog = keyIsDown(38)
-  if(OmhoogTwee === false && Omhoog === true ) {spelerY -= 270;
-   vijandY = spelerY - 270                                            }
-   
-  OmlaagTwee = Omlaag
+  if(OmhoogT === false && Omhoog === true ) {sY -= 270;
+   }
+  OmlaagT = Omlaag
   Omlaag = keyIsDown(40)
-  if(OmlaagTwee === false && Omlaag === true ) {spelerY += 270;}
-  // vijand
-  vijandX -= 20
-  healthX -= 10
-  if (healthX < 0) {
-    healthX = 1280;}
-    if (vijandX <0)
-    { vijandX = 1280
-    // vM = true
-      vijandY = Pos [Math.floor(Math.random() * Pos.length)]
+  if(OmlaagT === false && Omlaag === true ) {sY += 270;}
+  // lvl
+  if(HP>5 ){vXT -= 20
+            hX-= 18}
+    else {vXT = 1400}
+    if (HP >10) {vXT -= 5
+                      vX -= 5
+      hXT -=20}
+      else {hXT=1350}
+    if (HP > 15) {vXT -= 5
+                       vX-= 5
+                       hX -= 5
+    }
+  else if (HP > 20 ) {vXT -= 5
+                      vX -=5
+                      hXt-=5
+                      hX -=5}
+    if (HP > 25 ) { vXD -=45
+                         vXT -= 5
+                        vX -=5
+                        hXT-=5
+                        hX -=5}
+      else {vXD = 1400}
+    
+  
+ //v
+  vX -= 20
+    if (vX <0)
+    { vX = 1280
+      vY = Pos [Math.floor(Math.random() * Pos.length)]
   }
+    if (vXT<0 )
+    { vXT = 1350
+      vYT = Pos [Math.floor(Math.random() * Pos.length)]
+  }
+   if (vXD<0 )
+    { vXD = 1400
+      vYD = Pos [Math.floor(Math.random() * Pos.length)]
+  }
+  //h
+  hX -= 10
+  if (hX < 0) {
+    hX = 1280
+   hY =  Pos [Math.floor(Math.random() * Pos.length)]}
+  
+  if (hXT < 0) {
+    hXT = 1350
+   hYT =  Pos [Math.floor(Math.random() * Pos.length)]}
+  // IMG
  
-   
-  // kogel
-};
 if (imgX <-8720) {
     imgX = 0;}
  imgX -= 5
-
-
-        
-       
-
+   if (imgTX <-1000) {
+     imgTX = 0
+     
+   }
+  imgTX -=5
+   ;}
 /**
  * Checkt botsingen
  * Verwijdert neergeschoten dingen
- * Updatet globale variabelen punten en health
+ * Updatet globale variabelen punten en h
  */
 var verwerkBotsing = function () {
-  // botsing speler tegen vijand
-if (spelerY - vijandY <50 &&
-    spelerY - vijandY >-50 &&
-    spelerX - vijandX <50 && 
-    spelerX - vijandX >-50)
+  // botsing s tegen v
+if (sY - vY <50 &&
+    sY - vY >-50 &&
+    sX - vX <50 && 
+    sX - vX >-50)
 {console.log("boem");
 HP -= 2
- vijandY = Pos [Math.floor(Math.random() * Pos.length)]
- ///vG = true
- vijandX = 1280
+ vY = Pos [Math.floor(Math.random() * Pos.length)]
+ vX = 1280
 }
-  
- //if (vG== true) {vijandY = Pos [Math.floor(Math.random() * Pos.length)]}
-  // botsing kogel tegen vijand
- /*if (vM == true) {vijandY = 280 } { vijandY = Pos [Math.floor(Math.random() * Pos.length)] }
-  for (var i = 0; i < Pos.length; i+ 0) {  ellipse(vijandX, Pos[i], 60, 60);}*/
+  if (sY - vYT <50 &&
+    sY - vYT >-50 &&
+    sX - vXT <50 && 
+    sX - vXT >-50)
+{console.log("boem");
+HP -= 2
+ vYT = Pos [Math.floor(Math.random() * Pos.length)]
+ vXT = 1350 }
+ if (sY - vYD <50 &&
+    sY - vYD >-50 &&
+    sX - vXD <50 && 
+    sX - vXD >-50)
+{console.log("boem");
+HP -= 4
+ vYD = Pos [Math.floor(Math.random() * Pos.length)]
+ vXD = 1400 }
  
   
-  // update punten en health
-  if (spelerY - healthY <50 &&
-    spelerY - healthY >-50 &&
-   spelerX - healthX <50 &&
-    spelerX - healthX >-50) {
+  // update punten en h
+  if (sY - hY <50 &&
+    sY - hY >-50 &&
+   sX - hX <50 &&
+    sX - hX >-50) {
     console.log("hartje ");
     HP += 1
-    healthX =1300
-   // vM = true
+    hX =1300
+    hY =  Pos [Math.floor(Math.random() * Pos.length)]
   }
- 
-    if (HP < 1)  {
+  if (sY - hYT <50 &&
+    sY - hYT >-50 &&
+   sX - hXT <50 &&
+    sX - hXT >-50) {
+    console.log("hartje ");
+    HP += 2
+    hXT =1350
+    hYT =  Pos [Math.floor(Math.random() * Pos.length)]
+  }
+ if (HP < 0)  {
       spelStatus = GAMEOVER;
-    
   }
 };
 
@@ -126,60 +179,42 @@ var tekenAlles = function draw() {
   fill(150, 250, 50);
   rect(0, 0, width + 2 * 20, height + 2 * 20);
   image(img,imgX,-1650,10000,4000);
-  if(spelerX < 25) {spelerX = spelerX + 25}
-  if(spelerX > 1255) {spelerX = spelerX - 10}
-  if(spelerY < 25) {spelerY = spelerY + 30}
-  if(spelerY > 620) {spelerY = spelerY - 45}
-  rect (0,140,1450,50)
-  rect (0,410,1450,50)
-  rect (0,680,1450,50)
+  image(imgT,imgTX,410,1450,50)
+  for (var i=0 ; i<20 ; i++){
+    var imgTY = 140 + (i * 270);
+   image(imgT,imgTX,imgTY,3450,50);}
+  if(sX < 25) {sX = sX + 25}
+  if(sX > 1255) {sX = sX - 10}
+  if(sY < 25) {sY = sY + 30}
+  if(sY > 620) {sY = sY - 45}
+  //rect (0,140,1450,50)
+  //rect (0,410,1450,50)
+  //rect (0,680,1450,50)
 
 
   
-  // vijand
+  // v
 fill("red");
-  ellipse(vijandX, vijandY , 60,60)
-
-
-  
-
-
-    if (vijandX <0)
-    { vijandX = 1280
-    
-       // Pos [Math.floor(Math.random() * Pos.length)] = vijandY;
-        //newInd++;
-    }
-    
-    
-    /*fill("red");
-  ellipse(vijandX, vijandY , 60,60)
-   for (var i = 0; i < xPositions.length; i++) { 
-        ellipse(vijandX, Pos[i], 60, 60);}*/
-  { 
-       
-        ellipse(vijandX,vijandY, 60, 60);
-    }
-    
+  ellipse(vX, vY , 60,60)
+  ellipse(vXT,vYT,60,60)
+  fill("orange");
+  ellipse(vXD, vYD , 60,60)
   // kogel
-  fill("red")
-textSize (30)
-text(Pos,200,50)
-  // speler
+  
+  // s
    fill("white");
-  rect(spelerX, spelerY + 70, 10, 30)
-  rect(spelerX - 10, spelerY + 70, 10, 30)
- rect(spelerX - 15, spelerY + 20, 30, 50)
-  ellipse(spelerX, spelerY , 50, 50)
+  rect(sX, sY + 70, 10, 30)
+  rect(sX - 10, sY + 70, 10, 30)
+ rect(sX - 15, sY + 20, 30, 50)
+  ellipse(sX, sY , 50, 50)
   
-
-  
-
-  // punten en health
+  // punten en h
 fill ("yellow");
-  ellipse(healthX, healthY, 30,30)
+  ellipse(hX, hY, 30,30)
   textSize(30)
   text(HP,100,25);
+fill("green")
+ ellipse(hXT, hYT, 30,30)
 }; 
 
 /**
@@ -192,6 +227,7 @@ var checkGameOver = function () {
 };
 function preload () {
  img = loadImage('./fotos/achter.webp');
+  imgT = loadImage('./fotos/grond.png');
 }
 /* ********************************************* */
 /* setup() en draw() functies / hoofdprogramma   */
@@ -217,7 +253,7 @@ function setup() {
  */
 function draw() {
   if (spelStatus === SPELEN) {
-     beweegAlles();
+     begAlles();
     verwerkBotsing();
     tekenAlles();
     if (checkGameOver()) {
